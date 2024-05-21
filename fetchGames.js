2f26683a5f78fd4c14ac63793d5b45f0a8fd11d9
Xavier <xavierpriceau@gmail.com>
2024-05-21 20:45:05 +1000

    update html, css, and fetchGames.js to include filters
    switched from axios to fetch

--- fetchGames.js
+++ fetchGames.js
@@ -1,21 +1,47 @@
-const axios = require("axios");
-const getToken = require("./getToken");
-
-const fetchGames = async () => {
+import fetch from "node-fetch";
+import getToken from "./getToken.js";
+import { config } from "dotenv";
+config();
+// add parameters from user to fetch games
+async function fetchGames(search, rating) {
   try {
     const accessToken = await getToken();
-    const response = await axios.post("https://api.igdb.com/v4/games", null, {
+// use query for IGDB API
+    let query = `
+      fields name, total_rating;
+      search "${search}";
+      limit 10;
+    `;
+// add filters to query
+    if (rating !== "all") {
+      query += ` where total_rating >= ${rating};`;
+    }
+
+    const response = await fetch("https://api.igdb.com/v4/games", {
+      method: "POST",
       headers: {
         "Client-ID": process.env.CLIENT_ID,
         Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "text/plain",
       },
-      data: `fields name, involved_companies; search "Mario"; where version_parent = null; limit 10;`,
+      body: query,
     });
 
-    console.log("Games:", response.data);
-  } catch (error) {
-    console.error("Error fetching games:", error.response.data);
-  }
-};
+    if (!response.ok) {
+      throw new Error("Error fetching games");
+    }
 
-fetchGames();
+    const responseData = await response.json();
+    console.log("Games:", responseData);
+    return responseData;
+  } catch (error) {
+    console.error("Error fetching games:", error.message);
+    throw error;
+  }
+}
+
+//testing parameters
+////will remove once all filters have functions to extract data
+fetchGames("Mario", 60);
+
+export default fetchGames;
