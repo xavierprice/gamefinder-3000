7f03051a1984ab333c84c614594facb731c82706
Xavier <xavierpriceau@gmail.com>
2024-05-22 15:00:31 +1000

    switch to server-side environment for API requests

--- fetchGames.js
+++ fetchGames.js
@@ -1,47 +1,47 @@
-import fetch from "node-fetch";
-import getToken from "./getToken.js";
-import { config } from "dotenv";
-config();
+//use require for server-side environment
+const getToken = require("./getToken");
+//use axios instead of fetch, as fetch is not available in Node.js.
+const axios = require("axios");
+require("dotenv").config();
+//process clientId here for easier reasability
+const clientId = process.env.CLIENT_ID;
 // add parameters from user to fetch games
 async function fetchGames(search, rating) {
   try {
     const accessToken = await getToken();
-// use query for IGDB API
+    // use query for IGDB API
     let query = `
       fields name, total_rating;
       search "${search}";
       limit 10;
     `;
-// add filters to query
+    // add filters to query
     if (rating !== "all") {
       query += ` where total_rating >= ${rating};`;
     }
 
-    const response = await fetch("https://api.igdb.com/v4/games", {
-      method: "POST",
+    const response = await axios.post("https://api.igdb.com/v4/games", query, {
       headers: {
-        "Client-ID": process.env.CLIENT_ID,
+        //use variable here instead of process.env
+        "Client-ID": clientId,
         Authorization: `Bearer ${accessToken}`,
         "Content-Type": "text/plain",
       },
-      body: query,
     });
 
-    if (!response.ok) {
-      throw new Error("Error fetching games");
-    }
-
-    const responseData = await response.json();
-    console.log("Games:", responseData);
-    return responseData;
+    return response.data;
   } catch (error) {
     console.error("Error fetching games:", error.message);
     throw error;
   }
 }
+// testing parameters
+fetchGames("Mario", 90)
+  .then((responseData) => {
+    console.log("Games:", responseData);
+  })
+  .catch((error) => {
+    console.error("Error:", error.message);
+  });
 
-//testing parameters
-////will remove once all filters have functions to extract data
-fetchGames("Mario", 60);
-
-export default fetchGames;
+module.exports = fetchGames;
